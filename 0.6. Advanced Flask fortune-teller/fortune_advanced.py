6a090c53dc5e16b19030707b35387104bbfe3e7f
danaal25-meet <danaalabed0@gmail.com>
2024-07-21 12:47:28 +0300

    6th and 7th labs

---
+++ 0.6. Advanced Flask fortune-teller/fortune_advanced.py
@@ -0,0 +1,49 @@
+from flask import Flask , render_template, request, url_for, redirect
+import random
+
+app = Flask(__name__)
+
+fortunes = [
+        "You will have a great day!",
+        "Something unexpected will happen today.",
+        "You will achieve your goals.",
+        "Happiness is coming your way.",
+        "Be cautious of new opportunities.",
+        "A new friendship will bring joy to your life.",
+        "You will find success in your career.",
+        "Love is around the corner.",
+        "A pleasant surprise is waiting for you.",
+        "Your hard work will soon pay off."
+    ]
+
+
+
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+# @app.route('/fortune')
+# def fortune():
+#     selected_fortune = random.choice(fortunes)
+#     return render_template('fortune.html', fortune=selected_fortune)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        name = request.form['tt']
+        return redirect(url_for('fortune', n=name))
+    return render_template("hello.html")
+
+@app.route('/fortune/<n>')
+def fortune(n):
+    if len(n) < 10:
+        random_fortune = fortunes[len(n)]
+    else:
+        random_fortune = random.choice(fortunes)
+    return render_template("fortune.html", name=n, fortune=random_fortune)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True,port = 5003)
